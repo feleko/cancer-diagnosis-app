2dd11b2183f4f3d6ed3a8af47570011fdc16e0c1
Filip <filip.wozniczak@gmail.com>
2018-04-24 23:37:29 +0200

    zapisywanie informacji w skatalogowanych folderach

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 # Load libraries
 import pandas
+from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 import os
 import csv
-
-
-def cvs_reader(path):
-    reader = csv.reader(open(path), delimiter=' ', quotechar='|')
-    list_csv = list(reader)
-    list_csv.pop(0)
-    return list_csv
+import sys
 
 
 def test_libraries():
@@ -33,6 +28,44 @@ def test_libraries():
     print('sklearn: {}'.format(sklearn.__version__))
 
 
+def cvs_reader(path):
+    reader = csv.reader(open(path), delimiter=' ', quotechar='|')
+    list_csv = list(reader)
+    list_csv.pop(0)
+    return list_csv
+
+
+def create_chart(dataset, path):
+    # box and whisker plots
+    dataset.plot(kind='box', layout=(2, 2), sharex=False, sharey=False)
+    # plt.show()
+    plt.savefig('%s/box.png' % path)
+    plt.close()
+
+    # histograms
+    dataset.hist()
+    # plt.show()
+    plt.savefig('%s/histograms.png' % path)
+    plt.close()
+
+    # scatter plot matrix
+    scatter_matrix(dataset)
+    # plt.show()
+    plt.savefig('%s/scatter_matrix.png' % path)
+    plt.close()
+
+
+def statistical_data(dataset, fout=sys.stdout):
+    to_write = [('shape', dataset.shape), ('head', dataset.head(20))]
+
+    for name in 'count min max mean median quantile'.split():
+        to_write.append((name, getattr(dataset, name)()))
+        to_write.append(('descriptions', dataset.describe()))
+    s = '\n'.join(["%s: %s " % (str(i[0]), repr(i[1])) for i in to_write])
+    # s.write(fout)
+    fout.write(s)
+
+
 def main():
     test_libraries()
     path = 'csv'
@@ -45,32 +78,16 @@ def main():
                 dataset = dataset.drop(['No', 'Filename', 'CircularShapeFactor', 'ElongationA', 'ElongationB',
                                         'Roundness', 'Convex Defficiency', 'Eccentricity',
                                         'Perimeter', 'MeanIntensity', 'MinIntensity', 'MaxIntensity'], axis=1)
-                # shape
-                print(dataset.shape)
 
-                # head
-                print(dataset.head(20))
+                path = os.path.join("./scores/", fname)
+                if not os.path.exists(path):
+                    os.makedirs(path)
 
-                # descriptions
-                print(dataset.describe())
+                    create_chart(dataset, path)
 
-                # box and whisker plots
-                dataset.plot(kind='box', layout=(2, 2), sharex=False, sharey=False)
-                # plt.show()
-                plt.savefig('./img/box/(%s).png' % fname)
-                plt.close()
+                    with open(os.path.join(path, 'info.txt'), 'w') as fout:
+                        statistical_data(dataset, fout)
 
-                # histograms
-                dataset.hist()
-                # plt.show()
-                plt.savefig('./img/histograms/(%s).png' % fname)
-                plt.close()
-
-                # scatter plot matrix
-                pandas.scatter_matrix(dataset)
-                # plt.show()
-                plt.savefig('./img/scatter/(%s).png' % fname)
-                plt.close()
             else:
                 print("The file is empty: " + fname)
 
